32e3e4f2a39644e9342cf9d6ee831eb09a4c3c96
Jerots <jeronimotsv@gmail.com>
2025-01-13 12:40:41 +0100

    Add información de selección de voto al usuario

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const ratingButtons = document.querySelectorAll(".ratingButton");
 const submitButton = document.querySelector(".submitButton");
 const thankYouMessage = document.querySelector(".thankyouForVote");
+const selectedRatingText = document.querySelector(".selectedRatingText");
 
 let selectedRating = null;
 
@@ -16,6 +17,7 @@ ratingButtons.forEach((button) => {
 submitButton.addEventListener("click", () => {
     if (selectedRating) {
         document.querySelector(".ratingCard").style.display = "none";
+        selectedRatingText.textContent = `Seleccionaste ${selectedRating} de 5`;
         thankYouMessage.style.display = "block";
     } else {
         alert("¡Por favor, selecciona una calificación antes de enviar!");
